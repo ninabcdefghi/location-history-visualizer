a819c5346078749f698b3d14119dc783fd4c4cef
nportfolio <44119797+chaseinstead@users.noreply.github.com>
2019-01-05 16:54:23 +0100

    json parsing works

--- python-location-history-visualizer.py
+++ python-location-history-visualizer.py
@@ -1,14 +1,24 @@
 import argparse
 import json
 
+
+def prepare_location_history(json_file):
+	'''takes any google location history and formats it so its data points can be evaluated'''
+	with open(str(json_file)) as f:
+		info = f.read()
+		info = json.loads(info)
+    
+	try:
+		info = info["locations"] 
+	except:
+		pass
+
+	return info
+
 parser = argparse.ArgumentParser(description="Enter your json file.")
 parser.add_argument('--infile', nargs=1,
-                    help="JSON file to be processed",
-                    type=argparse.FileType('r'))
-
+                    type=str) #type=str?
 arguments = parser.parse_args()
 
-# TO DO: parse it as json...
-d = vars(arguments)
-
-print(d)
+loc_hist = prepare_location_history(arguments.infile[0])
+print(loc_hist) # parsed json
